28c3bef000cf68f8f09f5c10b47f5bb1a8ba776c
ElFloww <florent.dubut@gmail.com>
2024-03-09 12:44:31 +0100

    fix generated file

--- R5.08.Project.Database/DbProjectContext.cs
+++ R5.08.Project.Database/DbProjectContext.cs
@@ -4,7 +4,7 @@ namespace R5._08.Project.Database
 {
     public class DbProjectContext : R508projetContext
     {
-        public DbProjectContext(DbContextOptions<DbProjectContext> options) : base(options)
+        public DbProjectContext(DbContextOptions<DbProjectContext> p_Options) : base(p_Options)
         {
 
         }
